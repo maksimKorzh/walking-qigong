0947fe69844854c1c4af35969e76b8b2c31821b0
Code Monkey King <freesoft.for.people@gmail.com>
2025-02-08 14:18:00 +0200

    added CMK set

--- filters.py
+++ filters.py
@@ -4,298 +4,348 @@ steps = [
     'style': 'qigong',
     'goal': 'invocation',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'empty',
     'style': 'taiji',
     'goal': 'workout',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'kaihe_dantian',
     'style': 'qigong',
     'goal': 'workout',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'carry_bowl',
     'style': 'bagua',
     'goal': 'relaxation',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'carry_balls',
     'style': 'bagua',
     'goal': 'relaxation',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'twisting_waist',
     'style': 'qigong',
     'goal': 'invocation',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'forward_weight',
     'style': 'bagua',
     'goal': 'workout',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'backward',
     'style': 'bagua',
     'goal': 'workout',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'baikou_short',
     'style': 'bagua',
     'goal': 'workout',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'baikou_long',
     'style': 'bagua',
     'goal': 'workout',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'ski',
     'style': 'taiji',
     'goal': 'relaxation',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'heaven_earth',
     'style': 'qigong',
     'goal': 'workout',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'wuxin',
     'style': 'qigong',
     'goal': 'invocation',
     'uniquness': 'derivative',
+    'cmk': 'True',
   },
   {
     'name': 'pull_heel',
     'style': 'bagua',
     'goal': 'workout',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'push_toe',
     'style': 'bagua',
     'goal': 'workout',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'knee_top_head',
     'style': 'qigong',
     'goal': 'coordination',
     'uniquness': 'original',
+    'cmk': 'False',
   },
   {
     'name': 'lift_hip',
     'style': 'qigong',
     'goal': 'workout',
     'uniquness': 'original',
+    'cmk': 'False',
   },
   {
     'name': 'backward_bowing',
     'style': 'qigong',
     'goal': 'workout',
     'uniquness': 'derivative',
+    'cmk': 'True',
   },
   {
     'name': 'quick_backward',
     'style': 'xiaoyao',
     'goal': 'relaxation',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'tied_knees',
     'style': 'xiaoyao',
     'goal': 'relaxation',
     'uniquness': 'original',
+    'cmk': 'False',
   },
   {
     'name': 'flying_crane',
     'style': 'qigong',
     'goal': 'workout',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'horizontal_circles',
     'style': 'taiji',
     'goal': 'relaxation',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'crane',
     'style': 'qigong',
     'goal': 'invocation',
     'uniquness': 'derivative',
+    'cmk': 'True',
   },
   {
     'name': 'lungs_gut',
     'style': 'qigong',
     'goal': 'workout',
     'uniquness': 'derivative',
+    'cmk': 'True',
   },
   {
     'name': 'twist_waist_look_back',
     'style': 'qigong',
     'goal': 'invocation',
     'uniquness': 'derivative',
+    'cmk': 'True',
   },
   {
     'name': 'gaze_moon',
     'style': 'qigong',
     'goal': 'invocation',
     'uniquness': 'derivative',
+    'cmk': 'True',
   },
   {
     'name': 'five_hearts_breathing',
     'style': 'qigong',
     'goal': 'workout',
     'uniquness': 'derivative',
+    'cmk': 'True',
   },
   {
     'name': 'knee_bow',
     'style': 'qigong',
     'goal': 'workout',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'elbow_knee',
     'style': 'qigong',
     'goal': 'workout',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'one_swing',
     'style': 'xiaoyao',
     'goal': 'relaxation',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'two_swings',
     'style': 'xiaoyao',
     'goal': 'relaxation',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'half_swing',
     'style': 'xiaoyao',
     'goal': 'relaxation',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'fly_swing',
     'style': 'xiaoyao',
     'goal': 'relaxation',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'care_free',
     'style': 'xiaoyao',
     'goal': 'relaxation',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'arms_ropes',
     'style': 'xiaoyao',
     'goal': 'relaxation',
     'uniquness': 'original',
+    'cmk': 'False',
   },
   {
     'name': 'kaihe_heart',
     'style': 'qigong',
     'goal': 'workout',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'tripple_heater',
     'style': 'qigong',
     'goal': 'workout',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'rowing_boat',
     'style': 'taiji',
     'goal': 'workout',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'outward_rotation',
     'style': 'taiji',
     'goal': 'coordination',
     'uniquness': 'original',
+    'cmk': 'False',
   },
   {
     'name': 'inward_rotation',
     'style': 'taiji',
     'goal': 'coordination',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'body_rotation_forward',
     'style': 'taiji',
     'goal': 'workout',
     'uniquness': 'original',
+    'cmk': 'False',
   },
   {
     'name': 'body_rotation_backward',
     'style': 'taiji',
     'goal': 'workout',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'hand_foot_rotation_outward',
     'style': 'taiji',
     'goal': 'coordination',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'hand_foot_rotation_inward',
     'style': 'taiji',
     'goal': 'coordination',
     'uniquness': 'derivative',
+    'cmk': 'False',
   },
   {
     'name': 'throwing_ball',
     'style': 'taiji',
     'goal': 'relaxation',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'toe_heel_backward',
     'style': 'qigong',
     'goal': 'relaxation',
     'uniquness': 'original',
+    'cmk': 'True',
   },
   {
     'name': 'slap_knees',
     'style': 'qigong',
     'goal': 'relaxation',
     'uniquness': 'original',
+    'cmk': 'False',
   },
   {
     'name': 'mabu',
     'style': 'taiji',
     'goal': 'workout',
     'uniquness': 'original',
+    'cmk': 'False',
   },
 ]
 
 styles = ['bagua', 'taiji', 'taiji', 'qigong', 'xiayao']
 goals = ['invocation', 'workout', 'coordination', 'relaxation']
 uniqueness = ['original', 'derivative']
+cmk = ['True', 'False']
 
 def sort_by_style(select): return '\n'.join([[step['name'] for step in steps if step['style'] == select] for style in styles][0])
 def sort_by_goal(select): return '\n'.join([[step['name'] for step in steps if step['goal'] == select] for goal in goals][0])
 def sort_by_uniqueness(select): return '\n'.join([[step['name'] for step in steps if step['uniquness'] == select] for unique in uniqueness][0])
+def sort_by_cmk(select): return '\n'.join([[step['name'] for step in steps if step['cmk'] == select] for c in cmk][0])
 
 print('\n\n BY STYLE:')
 print('\n\nBagua steps (' + str(len(sort_by_style('bagua').split('\n'))) + ')\n\n' + sort_by_style('bagua'))
@@ -309,5 +359,8 @@ print('\n\nCoordination steps (' + str(len(sort_by_goal('coordination').split('\
 print('\n\nRelaxation steps (' + str(len(sort_by_goal('relaxation').split('\n'))) + ')\n\n' + sort_by_goal('relaxation'))
 print('\n\n BY UNIQUNESS:')
 print('\n\nOriginal steps (' + str(len(sort_by_uniqueness('original').split('\n'))) + ')\n\n' + sort_by_uniqueness('original'))
-print('\n\nOriginal steps (' + str(len(sort_by_uniqueness('derivative').split('\n'))) + ')\n\n' + sort_by_uniqueness('derivative'))
+print('\n\nDerivative steps (' + str(len(sort_by_uniqueness('derivative').split('\n'))) + ')\n\n' + sort_by_uniqueness('derivative'))
+print('\n\n BY CMK:')
+print('\n\nCMK steps (' + str(len(sort_by_cmk('True').split('\n'))) + ')\n\n' + sort_by_cmk('True'))
+print('\n\nOther steps (' + str(len(sort_by_cmk('False').split('\n'))) + ')\n\n' + sort_by_cmk('False'))
 print('\n\nTotal of ' + str(len(steps)) + ' steps\n\n')
