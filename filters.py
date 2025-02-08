66990ac78fd51cbadb4f3e330de0f0d00a1ff7e6
Code Monkey King <freesoft.for.people@gmail.com>
2025-02-08 14:05:42 +0200

    added more filters

--- filters.py
+++ filters.py
@@ -2,203 +2,312 @@ steps = [
   {
     'name': 'walking',
     'style': 'qigong',
+    'goal': 'invocation',
+    'uniquness': 'original',
   },
   {
     'name': 'empty',
     'style': 'taiji',
+    'goal': 'workout',
+    'uniquness': 'original',
   },
   {
     'name': 'kaihe_dantian',
     'style': 'qigong',
+    'goal': 'workout',
+    'uniquness': 'original',
   },
   {
     'name': 'carry_bowl',
     'style': 'bagua',
+    'goal': 'relaxation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'carry_balls',
     'style': 'bagua',
+    'goal': 'relaxation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'twisting_waist',
     'style': 'qigong',
+    'goal': 'invocation',
+    'uniquness': 'original',
   },
   {
     'name': 'forward_weight',
     'style': 'bagua',
+    'goal': 'workout',
+    'uniquness': 'derivative',
   },
   {
     'name': 'backward',
     'style': 'bagua',
+    'goal': 'workout',
+    'uniquness': 'original',
   },
   {
     'name': 'baikou_short',
     'style': 'bagua',
+    'goal': 'workout',
+    'uniquness': 'original',
   },
   {
     'name': 'baikou_long',
     'style': 'bagua',
+    'goal': 'workout',
+    'uniquness': 'derivative',
   },
   {
     'name': 'ski',
     'style': 'taiji',
+    'goal': 'relaxation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'heaven_earth',
     'style': 'qigong',
+    'goal': 'workout',
+    'uniquness': 'original',
   },
   {
     'name': 'wuxin',
     'style': 'qigong',
+    'goal': 'invocation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'pull_heel',
     'style': 'bagua',
+    'goal': 'workout',
+    'uniquness': 'derivative',
   },
   {
     'name': 'push_toe',
     'style': 'bagua',
+    'goal': 'workout',
+    'uniquness': 'derivative',
   },
   {
     'name': 'knee_top_head',
     'style': 'qigong',
+    'goal': 'coordination',
+    'uniquness': 'original',
   },
   {
     'name': 'lift_hip',
     'style': 'qigong',
+    'goal': 'workout',
+    'uniquness': 'original',
   },
   {
     'name': 'backward_bowing',
     'style': 'qigong',
+    'goal': 'workout',
+    'uniquness': 'derivative',
   },
   {
     'name': 'quick_backward',
     'style': 'xiaoyao',
+    'goal': 'relaxation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'tied_knees',
     'style': 'xiaoyao',
+    'goal': 'relaxation',
+    'uniquness': 'original',
   },
   {
     'name': 'flying_crane',
     'style': 'qigong',
+    'goal': 'workout',
+    'uniquness': 'derivative',
   },
   {
     'name': 'horizontal_circles',
     'style': 'taiji',
+    'goal': 'relaxation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'crane',
     'style': 'qigong',
+    'goal': 'invocation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'lungs_gut',
     'style': 'qigong',
+    'goal': 'workout',
+    'uniquness': 'derivative',
   },
   {
     'name': 'twist_waist_look_back',
     'style': 'qigong',
+    'goal': 'invocation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'gaze_moon',
     'style': 'qigong',
+    'goal': 'invocation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'five_hearts_breathing',
     'style': 'qigong',
+    'goal': 'workout',
+    'uniquness': 'derivative',
   },
   {
     'name': 'knee_bow',
     'style': 'qigong',
+    'goal': 'workout',
+    'uniquness': 'original',
   },
   {
     'name': 'elbow_knee',
     'style': 'qigong',
+    'goal': 'workout',
+    'uniquness': 'derivative',
   },
   {
     'name': 'one_swing',
     'style': 'xiaoyao',
+    'goal': 'relaxation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'two_swings',
     'style': 'xiaoyao',
+    'goal': 'relaxation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'half_swing',
     'style': 'xiaoyao',
+    'goal': 'relaxation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'fly_swing',
     'style': 'xiaoyao',
+    'goal': 'relaxation',
+    'uniquness': 'derivative',
   },
   {
     'name': 'care_free',
     'style': 'xiaoyao',
+    'goal': 'relaxation',
+    'uniquness': 'original',
   },
   {
     'name': 'arms_ropes',
     'style': 'xiaoyao',
+    'goal': 'relaxation',
+    'uniquness': 'original',
   },
   {
     'name': 'kaihe_heart',
     'style': 'qigong',
+    'goal': 'workout',
+    'uniquness': 'derivative',
   },
   {
     'name': 'tripple_heater',
     'style': 'qigong',
+    'goal': 'workout',
+    'uniquness': 'original',
   },
   {
     'name': 'rowing_boat',
     'style': 'taiji',
+    'goal': 'workout',
+    'uniquness': 'original',
   },
   {
     'name': 'outward_rotation',
     'style': 'taiji',
+    'goal': 'coordination',
+    'uniquness': 'original',
   },
   {
     'name': 'inward_rotation',
     'style': 'taiji',
+    'goal': 'coordination',
+    'uniquness': 'derivative',
   },
   {
     'name': 'body_rotation_forward',
     'style': 'taiji',
+    'goal': 'workout',
+    'uniquness': 'original',
   },
   {
     'name': 'body_rotation_backward',
     'style': 'taiji',
+    'goal': 'workout',
+    'uniquness': 'derivative',
   },
   {
     'name': 'hand_foot_rotation_outward',
     'style': 'taiji',
+    'goal': 'coordination',
+    'uniquness': 'derivative',
   },
   {
     'name': 'hand_foot_rotation_inward',
     'style': 'taiji',
+    'goal': 'coordination',
+    'uniquness': 'derivative',
   },
   {
     'name': 'throwing_ball',
     'style': 'taiji',
+    'goal': 'relaxation',
+    'uniquness': 'original',
   },
   {
     'name': 'toe_heel_backward',
     'style': 'qigong',
+    'goal': 'relaxation',
+    'uniquness': 'original',
   },
   {
-    'name': 'relax_legs',
+    'name': 'slap_knees',
     'style': 'qigong',
+    'goal': 'relaxation',
+    'uniquness': 'original',
   },
   {
     'name': 'mabu',
     'style': 'taiji',
+    'goal': 'workout',
+    'uniquness': 'original',
   },
 ]
 
 styles = ['bagua', 'taiji', 'taiji', 'qigong', 'xiayao']
+goals = ['invocation', 'workout', 'coordination', 'relaxation']
+uniqueness = ['original', 'derivative']
 
 def sort_by_style(select): return '\n'.join([[step['name'] for step in steps if step['style'] == select] for style in styles][0])
+def sort_by_goal(select): return '\n'.join([[step['name'] for step in steps if step['goal'] == select] for goal in goals][0])
+def sort_by_uniqueness(select): return '\n'.join([[step['name'] for step in steps if step['uniquness'] == select] for unique in uniqueness][0])
 
+print('\n\n BY STYLE:')
 print('\n\nBagua steps (' + str(len(sort_by_style('bagua').split('\n'))) + ')\n\n' + sort_by_style('bagua'))
 print('\n\nTaiji steps (' + str(len(sort_by_style('taiji').split('\n'))) + ')\n\n' + sort_by_style('taiji'))
 print('\n\nQigong steps (' + str(len(sort_by_style('qigong').split('\n'))) + ')\n\n' + sort_by_style('qigong'))
 print('\n\nXiaoyao steps (' + str(len(sort_by_style('xiaoyao').split('\n'))) + ')\n\n' + sort_by_style('xiaoyao'))
+print('\n\n BY GOAL:')
+print('\n\nInvocation steps (' + str(len(sort_by_goal('invocation').split('\n'))) + ')\n\n' + sort_by_goal('invocation'))
+print('\n\nWorkout steps (' + str(len(sort_by_goal('workout').split('\n'))) + ')\n\n' + sort_by_goal('workout'))
+print('\n\nCoordination steps (' + str(len(sort_by_goal('coordination').split('\n'))) + ')\n\n' + sort_by_goal('coordination'))
+print('\n\nRelaxation steps (' + str(len(sort_by_goal('relaxation').split('\n'))) + ')\n\n' + sort_by_goal('relaxation'))
+print('\n\n BY UNIQUNESS:')
+print('\n\nOriginal steps (' + str(len(sort_by_uniqueness('original').split('\n'))) + ')\n\n' + sort_by_uniqueness('original'))
+print('\n\nOriginal steps (' + str(len(sort_by_uniqueness('derivative').split('\n'))) + ')\n\n' + sort_by_uniqueness('derivative'))
 print('\n\nTotal of ' + str(len(steps)) + ' steps\n\n')
